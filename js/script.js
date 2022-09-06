13fe46f7ffaeb18353cd9ab851be0e194c66e7b7
Christopher <olanochristopher@gmail.com>
2022-09-06 22:56:12 +0800

    Add effect to storyBtn when click

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const headerBtn = document.querySelector(".header__btn");
+const storyBtn = document.querySelector(".btn__story");
 
-headerBtn.addEventListener("click", function (e) {
+const callback = (e) => {
   e.preventDefault();
 
   // Scroll to other links
   const sectionEl = document.querySelector(".homes");
   sectionEl.scrollIntoView({
     behavior: "smooth",
-    block: "nearest",
+    block: "center",
   });
-});
+};
+
+headerBtn.addEventListener("click", callback);
+storyBtn.addEventListener("click", callback);
